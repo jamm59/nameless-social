07f3b4a01d8db61a8d066480d48ee917affacbca
Favour <jammoben@gmail.com>
2025-10-29 19:37:45 +0000

    Made the main bottom page navigation bar look better

--- nameless-frontend/app/index.tsx
+++ nameless-frontend/app/index.tsx
@@ -1,12 +1,13 @@
 // app/index.tsx
 import { Stack } from 'expo-router';
-import { View, Text, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { View, Text, Image, ScrollView, TouchableOpacity, Animated } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { User, Search, Megaphone, Users, Plus, Scroll } from 'lucide-react-native';
 // import { Ionicons } from '@expo/vector-icons';
 import Feed from '~/components/Feed';
 import Stories from '~/components/Stories';
 import ProfileHeader from '~/components/ProfileHeader';
+import { useState, useRef, useEffect } from 'react';
 
 export default function Home() {
   const post: any[] = [
@@ -168,17 +169,86 @@ function NavIcon({ Icon, label, active = false }: { Icon: any; label?: string; a
   );
 }
 
+type NavItem = {
+  label: string;
+  Icon: any;
+};
+
+const navItems: NavItem[] = [
+  { label: 'Feed', Icon: User },
+  { label: 'Ads', Icon: Megaphone },
+  { label: 'Stories', Icon: Scroll },
+  { label: 'Search', Icon: Search },
+  // { label: 'Recommended', Icon: Users },
+];
+
 function FloatingNavigationBar() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const translateX = useRef(new Animated.Value(0)).current;
+
+  const [sliderDimensions, setSliderDimensions] = useState<{
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+  }>({
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+  });
+
+  function find_dimesions(event: any) {
+    const { x, y, width, height } = event.nativeEvent.layout;
+    setSliderDimensions({
+      x,
+      y,
+      width: Math.floor(width),
+      height: Math.floor(height),
+    });
+  }
+
+  const handlePress = (index: number) => {
+    setActiveIndex(index);
+    Animated.spring(translateX, {
+      toValue: index * sliderDimensions.width,
+      useNativeDriver: true,
+      friction: 8,
+    }).start();
+  };
+
   return (
-    <View className="absolute bottom-8 left-0 right-0 flex flex-row items-center justify-around">
-      <View className="w-[93%] flex-row items-center justify-around rounded-[2rem] bg-white py-3">
-        <NavIcon Icon={User} label="Feed" active />
-        <NavIcon Icon={Scroll} label="Stories" />
-        <NavIcon Icon={Search} label="Search" />
-        <NavIcon Icon={Megaphone} label="Ads" />
-        <NavIcon Icon={Users} label="Recommended" />
+    <View className="absolute bottom-8 left-0 right-0 flex flex-row items-center justify-start">
+      <View className="relative flex w-[70%] flex-row items-center justify-around overflow-hidden rounded-[2rem] bg-white px-2 py-4">
+        <View className="absolute inset-0 p-2">
+          <Animated.View
+            onLayout={find_dimesions}
+            style={{
+              transform: [{ translateX }],
+            }}
+            className="h-full w-1/4 rounded-3xl bg-rose-400 p-2 shadow-lg"
+          />
+        </View>
+
+        {navItems.map((item, index) => (
+          <TouchableOpacity
+            key={index}
+            className="flex-1 items-center rounded-2xl"
+            onPress={() => handlePress(index)}
+            activeOpacity={0.8}>
+            <item.Icon color={activeIndex === index ? 'white' : 'black'} size={27} />
+            <Text className={`text-md ${activeIndex === index ? 'text-white' : 'text-black'}`}>
+              {item.label}
+            </Text>
+          </TouchableOpacity>
+        ))}
       </View>
-      {/* <*/}
+      <TouchableOpacity
+        className="absolute right-4 flex items-center rounded-full bg-white px-5 py-4"
+        activeOpacity={0.8}>
+        <Users color={'black'} size={27} />
+        <Text className={`text-md text-black`}>Explore</Text>
+      </TouchableOpacity>
     </View>
   );
 }
